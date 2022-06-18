318014323c94a3be922cd0bb6eb0ca05220b9f7b
cOpEtriNe <70139476+cOpEtriNe@users.noreply.github.com>
2022-06-18 22:58:37 +0100

    easier way to get alts
    opens a dump file on github for roblox alts

---
+++ DumpLoader.bat.bat
@@ -0,0 +1,10 @@
+@echo off
+color 0f
+title Dump Loader V1
+echo Starting dump 1 loader.
+echo Visit https://github.com/cOpEtriNe/RBLX-Acc-Dump If you want more alt accounts (there is seperate files for different alt accounts)
+timeout /t 8 >nul
+start https://raw.githubusercontent.com/cOpEtriNe/RBLX-Acc-Dump/main/alts.txt
+timeout /t 3 >nul 
+echo Enjoy using the alt accounts!
+pause
